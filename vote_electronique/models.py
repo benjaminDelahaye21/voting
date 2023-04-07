cf5f5f8b0f849423ff751a7cac99190eb1bf7b2f
bdelahaye <benjamin.delahaye@xt.leroymerlin.fr>
2023-04-07 23:57:19 +0200

    update

--- vote_electronique/models.py
+++ vote_electronique/models.py
@@ -16,10 +16,14 @@ class Election(models.Model):
 class Candidat(models.Model):
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    biographie = models.TextField()
 
 class Vote(models.Model):
     electeur = models.ForeignKey(Electeur, on_delete=models.CASCADE)
     election = models.ForeignKey(Election, on_delete=models.CASCADE)
     candidat = models.ForeignKey(Candidat, on_delete=models.CASCADE)
     date_heure = models.DateTimeField(auto_now_add=True)
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
 
