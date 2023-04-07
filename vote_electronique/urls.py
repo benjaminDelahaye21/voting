b79ba80537ccb3a02a41a2fb4afea789112ac4ab
bdelahaye <benjamin.delahaye@xt.leroymerlin.fr>
2023-04-07 23:53:30 +0200

    update

--- vote_electronique/urls.py
+++ vote_electronique/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.liste_elections, name='liste_elections'),
-    path('election/<int:election_id>/', views.details_election, name='details_election'),
-    path('election/<int:election_id>/vote/', views.vote, name='vote'),
+    path("inscription/", views.inscription, name="inscription"),
+    path("connexion/", views.connexion, name="connexion"),
+    path("deconnexion/", views.deconnexion, name="deconnexion"),
+    path("", views.accueil, name="accueil"),
 ]
-
--- vote_electronique/views.py
+++ vote_electronique/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Election, Candidat, Vote
 from .forms import ElectionForm, CandidatForm, VoteForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 def liste_elections(request):
     elections = Election.objects.all()
@@ -24,5 +27,32 @@ def vote(request, election_id):
         form = VoteForm()
     return render(request, 'vote_electronique/vote.html', {'form': form})
 
+def inscription(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("accueil")
+    else:
+        form = UserCreationForm()
+    return render(request, "inscription.html", {"form": form})
 
+def connexion(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("accueil")
+    else:
+        form = AuthenticationForm()
+    return render(request, "connexion.html", {"form": form})
+
+def deconnexion(request):
+    logout(request)
+    return redirect("connexion")
+
+def accueil(request):
+    return render(request, "accueil.html")
 # Create your views here.
