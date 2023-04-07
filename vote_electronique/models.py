7489260c1063a5bbef92703d847b5b3aa0492fa1
bdelahaye <benjamin.delahaye@xt.leroymerlin.fr>
2023-04-07 23:41:52 +0200

    update model

--- vote_electronique/models.py
+++ vote_electronique/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Electeur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    mot_de_passe = models.CharField(max_length=100)
+
 class Election(models.Model):
     titre = models.CharField(max_length=100)
     date_debut = models.DateTimeField()
     date_fin = models.DateTimeField()
+    candidats = models.ManyToManyField('Candidat')
 
 class Candidat(models.Model):
     nom = models.CharField(max_length=100)
-    biographie = models.TextField()
-    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    prenom = models.CharField(max_length=100)
 
 class Vote(models.Model):
-    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+    electeur = models.ForeignKey(Electeur, on_delete=models.CASCADE)
     election = models.ForeignKey(Election, on_delete=models.CASCADE)
     candidat = models.ForeignKey(Candidat, on_delete=models.CASCADE)
-    date_vote = models.DateTimeField(auto_now_add=True)
+    date_heure = models.DateTimeField(auto_now_add=True)
 
